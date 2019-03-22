2ba1a8110424bbafef32df0599570fa17b721adb
Archaeologist03 <omniiknight@hotmail.com>
2019-03-22 11:12:47 +0100

    Adding mongoDB/mongoose to app. addtodo route works fine with db now.

--- server.js
+++ server.js
@@ -2,43 +2,36 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const uuid = require('uuid');
+const mongoose = require('mongoose');
 
-const addtodo = require('./controllers/addTodo');
-const addDone = require('./controllers/addDone');
-const deleteTodo = require('./controllers/deleteTodo');
-const deleteDone = require('./controllers/deleteDone');
+const listRoutes = require('./routes/list');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const db = {
-  users: [
-    {
-      id: null,
-      name: '',
-      email: '',
-      password: '',
-      todo: [{ id: 1, name: 'TASK n1' }, { id: 5, name: 'ggggg' }],
-      done: [{ id: 2, name: 'TASK n2' }],
-    },
-  ],
-};
+app.use(listRoutes);
 
-app.get('/', (req, res) => res.json(db));
+// const db = {
+//   users: [
+//     {
+//       id: null,
+//       name: '',
+//       email: '',
+//       password: '',
+//       todo: [{ id: 1, name: 'TASK n1' }, { id: 5, name: 'ggggg' }],
+//       done: [{ id: 2, name: 'TASK n2' }],
+//     },
+//   ],
+// };
 
-app.post('/addtodo', (req, res) => addtodo.handleAddTodo(req, res, db));
-
-app.post('/adddone', (req, res) => addDone.handleAddDone(req, res, db));
-
-app.delete('/deletetodo/:id', (req, res) =>
-  deleteTodo.handleDeleteTodo(req, res, db),
-);
-
-app.delete('/deletedone/:id', (req, res) =>
-  deleteDone.handleDeleteDone(req, res, db),
-);
+// app.get('/', (req, res) => res.json(db));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Listeing on port: ${PORT}..`));
+
+const mongoDB =
+  'mongodb+srv://ilija:bandera123@cluster0-cyaw7.mongodb.net/todo?retryWrites=true';
+mongoose.connect(mongoDB, { useNewUrlParser: true }).then(res => {
+  app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+});

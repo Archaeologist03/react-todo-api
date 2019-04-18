07211038492bc6ef503e8cfdd49992cd57c56b8a
Archaeologist03 <omniiknight@hotmail.com>
2019-04-18 20:35:07 +0200

    (i think i got it now...) Changing ip whitelist to 0000. for heroku deploy.

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(doneRoutes);
 const PORT = process.env.PORT || 5000;
 
 const mongoURI =
-  'mongodb+srv://ilija:bandera123@cluster0-5t3aa.mongodb.net/test?retryWrites=true';
+  'mongodb+srv://ilija:bandera123@cluster0-5t3aa.mongodb.net/todo?retryWrites=true';
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
